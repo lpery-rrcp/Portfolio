df1ea81598ce97dcb8b6b4f528206d130cfa1006
Lance Pery <lpery@academic.rrc.ca>
2026-05-25 19:02:30 -0500

    initial commit for portfolio.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <title>My Portfolio</title>
+      </div>
+    </div>
+  );
+}
+
+export default App;
