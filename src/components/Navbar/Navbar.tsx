0d147cd1fb871a95737ebf2386dfee5d54dfc8ba
Lance Pery <lpery@academic.rrc.ca>
2026-05-29 17:23:45 -0500

    Added the navbar css.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <img src={logo} alt="Logo" className="logo" />
+    <nav className={styles.navbar}>
+      <img src={logo} alt="Logo" className={styles.logo} />
 
-      <h1>My Website</h1>
-      <ul>
+      <h1 className={styles.title}>My Website</h1>
+      <ul className={styles.navLinks}>
         <li>
           <Link to="/">Home</Link>
         </li>
